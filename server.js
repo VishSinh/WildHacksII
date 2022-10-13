513b28fba9b8ab8b70236fcdce7709b66ac5784c
vishsinh <vish.sinh.vs@gmail.com>
2022-10-13 14:18:31 +0530

    added backend files

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
-const express = require(`express`);
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const ejs = require("ejs");
+
+
+const upload = multer({
+    dest: "image/"
+});
 const app = express();
+
+let infoList = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
 
 
 app.get(`/`, function (req, res) {
-    res.sendFile(__dirname + `/index.html`)
+    res.render(`index`);
+});
+
+app.get(`/form`, function (req, res) {
+    res.render(`form`);
+});
+
+app.get(`/list`, function (req, res) {
+    res.render(`list`, {
+        infoList: infoList
+    });
+})
+
+app.post(`/form`, upload.single("image"), function (req, res) {
+    let info = {
+        animal: req.body.animal,
+        breed: req.body.breed,
+        location: req.body.location,
+        description: req.body.description,
+        phoneNumber: req.body.phoneNumber
+    }
+
+    console.log(req.body);
+    console.log(req.files);
+
+    console.log(info);
+
+    infoList.push(info);
+    res.redirect(`/`);
 })
 
 app.listen(3000, function () {
